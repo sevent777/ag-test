94dbc42fc725f11a3d5c433c459465cc32f4ad2d
Wukun Cai <wukun.cai@shopee.com>
2024-05-21 15:52:43 +0800

    feat: init

---
+++ src/index.tsx
@@ -0,0 +1,89 @@
+console.log('1 :>> ', 1);
+
+"use strict";
+
+import React, {
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+  StrictMode,
+} from "react";
+import { createRoot } from "react-dom/client";
+import { AgGridReact } from "@ag-grid-community/react";
+import "@ag-grid-community/styles/ag-grid.css";
+import "@ag-grid-community/styles/ag-theme-quartz.css";
+import {
+  ColDef,
+  ColGroupDef,
+  GridApi,
+  GridOptions,
+  GridReadyEvent,
+  ModuleRegistry,
+  createGrid,
+} from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { ClipboardModule } from "@ag-grid-enterprise/clipboard";
+import { MenuModule } from "@ag-grid-enterprise/menu";
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  ClipboardModule,
+  MenuModule,
+]);
+
+import { LicenseManager } from "@ag-grid-enterprise/core";
+
+LicenseManager.setLicenseKey(
+  "Using_this_{AG_Grid}_Enterprise_key_{AG-054770}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Eemont_Private_Limited}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{zf-zheng}_only_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_working_on_{zf-zheng}_need_to_be_licensed___{zf-zheng}_has_not_been_granted_a_Deployment_License_Add-on___This_key_works_with_{AG_Grid}_Enterprise_versions_released_before_{8_February_2025}____[v3]_[01]_MTczODk3MjgwMDAwMA==ab213676a302d8b6ee5c3b54f2751d0f"
+);
+
+const GridExample = () => {
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState<unknown[]>();
+  const [columnDefs, setColumnDefs] = useState<ColDef[]>([
+    { field: "athlete", minWidth: 150 },
+    { field: "age", maxWidth: 90 },
+    { field: "country", minWidth: 150 },
+    { field: "year", maxWidth: 90 },
+    { field: "date", minWidth: 150 },
+    { field: "sport", minWidth: 150 },
+    { field: "gold" },
+    { field: "silver" },
+    { field: "bronze" },
+    { field: "total" },
+  ]);
+  const defaultColDef = useMemo<ColDef>(() => {
+    return {
+      flex: 1,
+      minWidth: 100,
+    };
+  }, []);
+
+  const onGridReady = useCallback((params: GridReadyEvent) => {
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data: unknown[]) => setRowData(data));
+  }, []);
+
+  return (
+    <div style={containerStyle}>
+      <div style={gridStyle} className={"ag-theme-quartz-dark"}>
+        <AgGridReact<unknown>
+          rowData={rowData}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          enableRangeSelection={true}
+          onGridReady={onGridReady}
+        />
+      </div>
+    </div>
+  );
+};
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <StrictMode>
+    <GridExample />
+  </StrictMode>
+);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx','.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+  },
+};
